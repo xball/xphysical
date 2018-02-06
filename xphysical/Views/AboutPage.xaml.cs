02bfb134e00625e7ad09936b0f784656194292a7
Paul Wong <hpwong@gmail.com>
2018-02-06 23:58:25 +0800

    first update

---
+++ xphysical/Views/AboutPage.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+using Xamarin.Forms;
+
+namespace xphysical
+{
+    public partial class AboutPage : ContentPage
+    {
+        public AboutPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
